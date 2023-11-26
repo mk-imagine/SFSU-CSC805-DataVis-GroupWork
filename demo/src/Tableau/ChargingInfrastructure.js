58d90bbdcf3881360666b0204f9b1714f20481cf
Mark Kim <mkim22@mail.sfsu.edu>
2023-11-25 21:40:38 -0800

    responsive auto-resize complete

--- demo/src/Tableau/ChargingInfrastructure.js
+++ demo/src/Tableau/ChargingInfrastructure.js
@@ -8,7 +8,24 @@ function ChargingInfrastructure() {
   const initTableau = () => {
     const vizUrl =
       "https://public.tableau.com/views/inf_dash/inf_dash";
-    let viz = new window.tableau.Viz(vizContainer.current, vizUrl);
+    const options = {
+        height: window.innerHeight - 250,
+        width: window.innerWidth - 130,
+        hideToolbar: true,
+    }
+    let viz = new window.tableau.Viz(vizContainer.current, vizUrl, options);
+
+    function autoResize() {
+        const width = window.innerWidth - 130;
+        const height = window.innerHeight - 250;
+
+        viz.setFrameSize(width, height);
+    }
+
+    window.addEventListener('resize', () => {
+        console.log(`Resizing the window: ${window.innerHeight}, ${window.innerWidth}`);
+        autoResize();
+    })
   };
 
   useEffect(() => {
